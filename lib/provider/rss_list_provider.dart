b60ed104ff28bbce77da8b93219fe09f012dcffe
SelTeck <zbilizo@gmail.com>
2024-09-09 19:00:28 +0900

    provider 구조 적용 완료

---
+++ lib/provider/rss_list_provider.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:pain_record/commmon/define.dart';
+import 'package:pain_record/model/blogrss.dart';
+import 'package:pain_record/session/session.dart';
+
+class RssListProvider with ChangeNotifier {
+  var _page = 0;
+  // final _memoizer = AsyncMemoizer<List<BlogRss>>();
+  final List<BlogRss> _blogPageList = [];
+
+  set page(int page) => _page = page;
+  List<BlogRss> get blogPageList => _blogPageList;
+
+  Future<List<BlogRss>> fetchBlogRssList(String token) async {
+    _page += 1;
+
+    print('SelTeck ... called _getBlogRssInfo. page = $_page');
+    Session.headers['Authorization'] = 'Bearer $token';
+
+    final response = await Session.get(
+        url: '${Session.host}/records/list/$_page/${Defines.VIEW_COUNT}');
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => BlogRss.fromJson(data)).toList();
+    } else {
+      throw Exception('Failed to load items');
+    }
+  }
+}
---
+++ lib/widgets/rss_listview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class RssListview extends StatelessWidget {
+  const RssListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
