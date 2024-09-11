c9ea91b30c67e7e2548c136dff6cf1e20d143779
SelTeck <zbilizo@gmail.com>
2024-09-11 19:12:52 +0900

    import 변경

--- lib/views/rss_list_view.dart
+++ lib/views/rss_list_view.dart
@@ -6,7 +6,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:pain_record/model/blogrss.dart';
 import 'package:pain_record/provider/auth_provider.dart';
 import 'package:pain_record/provider/rss_list_provider.dart';
-import 'package:pain_record/views/blog_rss_row_view.dart';
+import 'package:pain_record/widgets/blog_rss_row_view.dart';
 import 'package:provider/provider.dart';
 
 class RssListView extends StatefulWidget {
@@ -64,51 +64,6 @@ class _RssListView extends State<RssListView> {
         },
       ),
     );
-    // return SafeArea(
-    //   child: FutureBuilder<List<BlogRss>>(
-    //     future: _provider.fetchBlogRssList(_token),
-    //     builder: (context, snapshot) {
-    //       switch (snapshot.connectionState) {
-    //         case ConnectionState.none:
-    //         // return Text('Press button to start.');
-    //         case ConnectionState.waiting:
-    //         case ConnectionState.active:
-    //         // return Text('Awaiting result...');
-    //         case ConnectionState.done:
-    //           {
-    //             if (snapshot.hasData) {
-    //               List<BlogRss> list = snapshot.data!;
-    //               _blogPageList.addAll(list);
-
-    //               return RefreshIndicator(
-    //                   key: _refreshIndicatorKey,
-    //                   child: ListView.separated(
-    //                     controller: _scrollController,
-    //                     separatorBuilder: (context, index) => const Divider(
-    //                       color: Colors.black,
-    //                     ),
-    //                     itemBuilder: (context, index) {
-    //                       return BlogRssRowView(item: _blogPageList[index]);
-    //                     },
-    //                     itemCount: _blogPageList.length,
-    //                   ),
-    //                   onRefresh: () async {
-    //                     setState(() {
-    //                       _provider.page = 0;
-    //                       _blogPageList.clear();
-    //                     });
-    //                   });
-    //             } else if (snapshot.hasError) {
-    //               throw Error.safeToString(snapshot.error);
-    //             }
-    //             return const Center(
-    //               child: CircularProgressIndicator(),
-    //             );
-    //           }
-    //       }
-    //     },
-    //   ),
-    // );
   }
 
   void onScroll() {
@@ -125,7 +80,7 @@ class _RssListView extends State<RssListView> {
     List<BlogRss> list = await _provider.fetchBlogRssList(_token);
     setState(() {
       _blogPageList.addAll(list);
-      EasyLoading.dismiss();
     });
+    EasyLoading.dismiss();
   }
 }
