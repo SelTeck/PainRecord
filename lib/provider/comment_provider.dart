b312a28c0745f38b2c93fbc75404ff7870084411
SelTeck <zbilizo@gmail.com>
2024-09-11 19:20:08 +0900

    comment provider 구조 적용

---
+++ lib/provider/comment_provider.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:pain_record/session/session.dart';
+import 'package:pain_record/model/result.dart';
+import 'package:pain_record/model/commentinfo.dart';
+
+class CommentProvider with ChangeNotifier {
+  // final AsyncMemoizer _morizer = AsyncMemoizer();
+
+  CommentInfo? _commentInfo;
+  CommentInfo? get commentInfo => _commentInfo;
+
+  Future<CommentInfo?> searchComment(int blogIndex) async {
+    final response = await Session.get(
+        url: '${Session.host}/records/data/daily/comment/$blogIndex');
+
+    if (response.statusCode == 200) {
+      return CommentInfo.fromJson(json.decode(response.body));
+    }
+    return null;
+  }
+
+  Future<String> updateComment(String params) async {
+    final response = await Session.put(
+        url: '${Session.host}/records/data/update/daily', body: params);
+
+    return Result.fromJson(jsonDecode(response.body)).message;
+  }
+
+  Future<String> saveComment(String params) async {
+    final response = await Session.post(
+        url: '${Session.host}/records/data/input/daily', body: params);
+
+    return Result.fromJson(jsonDecode(response.body)).message;
+  }
+}
