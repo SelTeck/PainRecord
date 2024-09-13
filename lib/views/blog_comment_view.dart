0acf6e624ab32eb69ca30880f5840d29b3815551
SelTeck <zbilizo@gmail.com>
2024-09-13 18:25:07 +0900

    UI comment_medicine widget 생성후  변경

--- lib/views/blog_comment_view.dart
+++ lib/views/blog_comment_view.dart
@@ -7,9 +7,10 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:pain_record/model/commentinfo.dart';
 import 'package:pain_record/provider/comment_provider.dart';
-import 'package:pain_record/widgets/comment_stimulator_dropdown_button.dart';
-import 'package:pain_record/widgets/comment_swell_dropdown_button.dart';
-import 'package:pain_record/widgets/comment_update_button.dart';
+import 'package:pain_record/widgets/blog_comment/comment_medicine_input.dart';
+import 'package:pain_record/widgets/blog_comment/comment_stimulator_dropdown_button.dart';
+import 'package:pain_record/widgets/blog_comment/comment_swell_dropdown_button.dart';
+import 'package:pain_record/widgets/blog_comment/comment_update_button.dart';
 
 class BlogCommentView extends StatefulWidget {
   final blogIndex;
@@ -20,11 +21,6 @@ class BlogCommentView extends StatefulWidget {
 }
 
 class _BlogCommentView extends State<BlogCommentView> {
-  final _isTakeMorning = false.obs;
-  final _isTakeDinner = false.obs;
-  final _isTakePatch = false.obs;
-  final _isAntiAnalgesic = false.obs;
-  final _isNarcoticAnalgesic = false.obs;
   final _isStimulusCharge = false.obs;
 
   final _swellingController = SwellingController();
@@ -36,6 +32,7 @@ class _BlogCommentView extends State<BlogCommentView> {
   late CommentSwellDropdownButton _swellDropdownButton;
   late CommentStimulatorDropdownButton _activeDropDownButton;
   late CommentStimulatorDropdownButton _sleepDrowWondButton;
+  late CommentMedicineInput _medicineInputBox;
 
   bool _isHaveData = false;
 
@@ -48,9 +45,17 @@ class _BlogCommentView extends State<BlogCommentView> {
         CommentStimulatorDropdownButton(controller: _activeController);
     _sleepDrowWondButton =
         CommentStimulatorDropdownButton(controller: _sleepController);
+
+    _medicineInputBox = CommentMedicineInput();
     super.initState();
 
     _searchCommentInfor();
+  }
+
+  @override
+  void dispose() {
+    _commentTextController.dispose();
+    super.dispose();
   }
 
   @override
@@ -62,14 +67,32 @@ class _BlogCommentView extends State<BlogCommentView> {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
         ),
         trailing: CommentUpdateButton(onPressed: () {
-          _isHaveData ? _updateComment() : _saveComment();
+          Map<String, dynamic> params = {
+            'crawlingIdx': widget.blogIndex,
+            'takeMorning': _medicineInputBox.isTakeMorning ? 1 : 0,
+            'takeEvening': _medicineInputBox.isTakeDinner ? 1 : 0,
+            'antiAnalgesic': _medicineInputBox.isAntiAnalgesic ? 1 : 0,
+            'narcoticAnalgesic': _medicineInputBox.isNarcoticAnalgesic ? 1 : 0,
+            'usePath': _medicineInputBox.isTakePatch ? 1 : 0,
+            'comment': _commentTextController.text,
+            'swelling':
+                _swellDropdownButton.list.indexOf(_swellingController.value),
+            'activeMode':
+                _activeDropDownButton.list.indexOf(_activeController.value),
+            'sleepMode':
+                _sleepDrowWondButton.list.indexOf(_sleepController.value),
+            'chargingStimulus': _isStimulusCharge.value ? 1 : 0,
+          };
+          _isHaveData
+              ? _updateComment(jsonEncode(params))
+              : _saveComment(jsonEncode(params));
         }),
       ),
       body: Expanded(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              _makeTakeMedicine(),
+              _medicineInputBox,
               Padding(
                 padding: const EdgeInsets.fromLTRB(5.0, 12.0, 5.0, 0.0),
                 child: TextField(
@@ -83,77 +106,6 @@ class _BlogCommentView extends State<BlogCommentView> {
               _makeStimulusInfo(),
             ],
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget _makeTakeMedicine() {
-    return Container(
-      margin: const EdgeInsets.only(top: 20),
-      padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 0.0),
-      child: InputDecorator(
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: '약 복용 특이 사항',
-          labelStyle: TextStyle(
-              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
-        child: Column(
-          children: [
-            Obx(
-              () => CheckboxListTile(
-                value: _isTakeMorning.value,
-                onChanged: (bool? value) {
-                  _isTakeMorning.value = value!;
-                },
-                title: const Text('아침 약 복용'),
-                subtitle: const Text('울트라셋 한 알 복용'),
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-            ),
-            Obx(
-              () => CheckboxListTile(
-                value: _isTakeDinner.value,
-                onChanged: (bool? value) {
-                  _isTakeDinner.value = value!;
-                },
-                title: const Text('저녁 약 복용'),
-                subtitle: const Text('울트라셋 한 알 복용'),
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-            ),
-            Obx(
-              () => CheckboxListTile(
-                value: _isTakePatch.value,
-                onChanged: (bool? value) {
-                  _isTakePatch.value = value!;
-                },
-                title: const Text('뉴도탑 패치 사용'),
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-            ),
-            Obx(
-              () => CheckboxListTile(
-                value: _isAntiAnalgesic.value,
-                onChanged: (bool? value) {
-                  _isAntiAnalgesic.value = value!;
-                },
-                title: const Text('소염진통제 복용'),
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-            ),
-            Obx(
-              () => CheckboxListTile(
-                value: _isNarcoticAnalgesic.value,
-                onChanged: (bool? value) {
-                  _isNarcoticAnalgesic.value = value!;
-                },
-                title: const Text('마약성 진통제 복용'),
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-            ),
-          ],
         ),
       ),
     );
@@ -252,12 +204,12 @@ class _BlogCommentView extends State<BlogCommentView> {
     CommentInfo? comment = await _provider.searchComment(widget.blogIndex);
     if (comment != null) {
       _isHaveData = true;
-      _isTakeMorning.value = comment.takeMorning;
-      _isTakeDinner.value = comment.takeEvening;
-      _isTakePatch.value = comment.usePath;
-      _isAntiAnalgesic.value = comment.takeAnalogesic;
-      _isNarcoticAnalgesic.value = comment.takeNarcotic;
-      _commentTextController.text = comment.comment;
+      _medicineInputBox.isTakeMorning = comment.takeMorning;
+      _medicineInputBox.isTakeDinner = comment.takeEvening;
+      _medicineInputBox.isTakePatch = comment.usePath;
+      _medicineInputBox.isAntiAnalgesic = comment.takeAnalogesic;
+      _medicineInputBox.isNarcoticAnalgesic = comment.takeNarcotic;
+      _commentTextController.text = comment.comment.toString();
       _swellingController.value = _swellDropdownButton.list[comment.swellingLv];
       _activeController.value = _activeDropDownButton.list[comment.activeMode];
       _sleepController.value = _sleepDrowWondButton.list[comment.sleepMode];
@@ -269,47 +221,19 @@ class _BlogCommentView extends State<BlogCommentView> {
     EasyLoading.dismiss();
   }
 
-  Future<void> _updateComment() async {
+  Future<void> _updateComment(String params) async {
     EasyLoading.show(status: "Updating Comment...");
 
-    Map<String, dynamic> params = {
-      'crawlingIdx': widget.blogIndex,
-      'takeMorning': _isTakeMorning.value ? 1 : 0,
-      'takeEvening': _isTakeDinner.value ? 1 : 0,
-      'antiAnalgesic': _isAntiAnalgesic.value ? 1 : 0,
-      'narcoticAnalgesic': _isNarcoticAnalgesic.value ? 1 : 0,
-      'usePath': _isTakePatch.value ? 1 : 0,
-      'comment': _commentTextController.text,
-      'swelling': _swellDropdownButton.list.indexOf(_swellingController.value),
-      'activeMode': _activeDropDownButton.list.indexOf(_activeController.value),
-      'sleepMode': _sleepDrowWondButton.list.indexOf(_sleepController.value),
-      'chargingStimulus': _isStimulusCharge.value ? 1 : 0,
-    };
-
-    var message = await _provider.updateComment(jsonEncode(params));
+    var message = await _provider.updateComment(params);
     Fluttertoast.showToast(msg: message);
 
     EasyLoading.dismiss();
   }
 
-  Future<void> _saveComment() async {
+  Future<void> _saveComment(String params) async {
     EasyLoading.show(status: "Saving Comment...");
 
-    Map<String, dynamic> params = {
-      'crawlingIdx': widget.blogIndex,
-      'takeMorning': _isTakeMorning.value ? 1 : 0,
-      'takeEvening': _isTakeDinner.value ? 1 : 0,
-      'antiAnalgesic': _isAntiAnalgesic.value ? 1 : 0,
-      'narcoticAnalgesic': _isNarcoticAnalgesic.value ? 1 : 0,
-      'usePath': _isTakePatch.value ? 1 : 0,
-      'comment': _commentTextController.text,
-      'swelling': _swellDropdownButton.list.indexOf(_swellingController.value),
-      'activeMode': _activeDropDownButton.list.indexOf(_activeController.value),
-      'sleepMode': _sleepDrowWondButton.list.indexOf(_sleepController.value),
-      'chargingStimulus': _isStimulusCharge.value ? 1 : 0,
-    };
-
-    var message = await _provider.saveComment(jsonEncode(params));
+    var message = await _provider.saveComment(params);
     Fluttertoast.showToast(msg: message);
 
     EasyLoading.dismiss();
