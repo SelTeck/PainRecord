fbd93c7b5c35da349f21df95ea84edd3634bc4e7
SelTeck <zbilizo@gmail.com>
2024-09-16 17:52:59 +0900

    UI comment widget 추출 및 적용

--- lib/views/blog_comment_view.dart
+++ lib/views/blog_comment_view.dart
@@ -4,13 +4,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:get/get.dart';
 import 'package:pain_record/model/commentinfo.dart';
 import 'package:pain_record/provider/comment_provider.dart';
-import 'package:pain_record/widgets/blog_comment/comment_medicine_input.dart';
-import 'package:pain_record/widgets/blog_comment/comment_stimulator_dropdown_button.dart';
-import 'package:pain_record/widgets/blog_comment/comment_swell_dropdown_button.dart';
-import 'package:pain_record/widgets/blog_comment/comment_update_button.dart';
+import 'package:pain_record/widgets/blog_comment/comment_button_update.dart';
+import 'package:pain_record/widgets/blog_comment/comment_widget_medicine_infor.dart';
+import 'package:pain_record/widgets/blog_comment/comment_widget_stimulus_infor.dart';
+import 'package:pain_record/widgets/blog_comment/comment_widget_swelling_infor.dart';
 
 class BlogCommentView extends StatefulWidget {
   final blogIndex;
@@ -21,32 +20,21 @@ class BlogCommentView extends StatefulWidget {
 }
 
 class _BlogCommentView extends State<BlogCommentView> {
-  final _isStimulusCharge = false.obs;
-
-  final _swellingController = SwellingController();
-  final _activeController = StimulatorController();
-  final _sleepController = StimulatorController();
   final _commentTextController = TextEditingController();
 
   late CommentProvider _provider;
-  late CommentSwellDropdownButton _swellDropdownButton;
-  late CommentStimulatorDropdownButton _activeDropDownButton;
-  late CommentStimulatorDropdownButton _sleepDrowWondButton;
-  late CommentMedicineInput _medicineInputBox;
+  late CommentWidgetMedicineInfor _medicineInforBox;
+  late CommentWidgetSwellingInfor _swellingInforBox;
+  late CommentWidgetStimulusInfor _stimulusInforBox;
 
   bool _isHaveData = false;
 
   @override
   void initState() {
     _provider = CommentProvider();
-    _swellDropdownButton =
-        CommentSwellDropdownButton(controller: _swellingController);
-    _activeDropDownButton =
-        CommentStimulatorDropdownButton(controller: _activeController);
-    _sleepDrowWondButton =
-        CommentStimulatorDropdownButton(controller: _sleepController);
-
-    _medicineInputBox = CommentMedicineInput();
+    _medicineInforBox = CommentWidgetMedicineInfor();
+    _swellingInforBox = CommentWidgetSwellingInfor();
+    _stimulusInforBox = CommentWidgetStimulusInfor();
     super.initState();
 
     _searchCommentInfor();
@@ -66,132 +54,40 @@ class _BlogCommentView extends State<BlogCommentView> {
           '추가 정보 입력',
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
         ),
-        trailing: CommentUpdateButton(onPressed: () {
+        trailing: CommentButtonUpdate(onPressed: () {
           Map<String, dynamic> params = {
             'crawlingIdx': widget.blogIndex,
-            'takeMorning': _medicineInputBox.isTakeMorning ? 1 : 0,
-            'takeEvening': _medicineInputBox.isTakeDinner ? 1 : 0,
-            'antiAnalgesic': _medicineInputBox.isAntiAnalgesic ? 1 : 0,
-            'narcoticAnalgesic': _medicineInputBox.isNarcoticAnalgesic ? 1 : 0,
-            'usePath': _medicineInputBox.isTakePatch ? 1 : 0,
+            'takeMorning': _medicineInforBox.isTakeMorning ? 1 : 0,
+            'takeEvening': _medicineInforBox.isTakeDinner ? 1 : 0,
+            'antiAnalgesic': _medicineInforBox.isAntiAnalgesic ? 1 : 0,
+            'narcoticAnalgesic': _medicineInforBox.isNarcoticAnalgesic ? 1 : 0,
+            'usePath': _medicineInforBox.isTakePatch ? 1 : 0,
             'comment': _commentTextController.text,
-            'swelling':
-                _swellDropdownButton.list.indexOf(_swellingController.value),
-            'activeMode':
-                _activeDropDownButton.list.indexOf(_activeController.value),
-            'sleepMode':
-                _sleepDrowWondButton.list.indexOf(_sleepController.value),
-            'chargingStimulus': _isStimulusCharge.value ? 1 : 0,
+            'swelling': _swellingInforBox.value,
+            'activeMode': _stimulusInforBox.activeValue,
+            'sleepMode': _stimulusInforBox.sleepValue,
+            'chargingStimulus': _stimulusInforBox.isStimulusCharge ? 1 : 0,
           };
           _isHaveData
               ? _updateComment(jsonEncode(params))
               : _saveComment(jsonEncode(params));
         }),
       ),
-      body: Expanded(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              _medicineInputBox,
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5.0, 12.0, 5.0, 0.0),
-                child: TextField(
-                  controller: _commentTextController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              _makeSwellingInfo(),
-              _makeStimulusInfo(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _makeSwellingInfo() {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(4.0, 10.0, 4.0, 4.0),
-      padding: const EdgeInsets.fromLTRB(16.0, 4.0, 4.0, 4.0),
-      decoration: BoxDecoration(
-        border:
-            Border.all(color: Colors.grey, style: BorderStyle.solid, width: 1),
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      child: Row(
-        children: [
-          const Expanded(
-            flex: 2,
-            child: Text(
-              '환부의 부기',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Expanded(
-            child: _swellDropdownButton,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _makeStimulusInfo() {
-    return Container(
-      margin: const EdgeInsets.only(top: 20),
-      padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 0.0),
-      child: InputDecorator(
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: '척수신경 자극기 설정',
-          labelStyle: TextStyle(
-              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              children: [
-                const Expanded(
-                  flex: 2,
-                  child: Text(
-                    '일상 생활 모드',
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
+            _medicineInforBox,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 12.0, 5.0, 0.0),
+              child: TextField(
+                controller: _commentTextController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
                 ),
-                Expanded(
-                  child: _activeDropDownButton,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                const Expanded(
-                  flex: 2,
-                  child: Text(
-                    '수면 모드',
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: _sleepDrowWondButton,
-                ),
-              ],
-            ),
-            Obx(
-              () => CheckboxListTile(
-                value: _isStimulusCharge.value,
-                onChanged: (bool? value) {
-                  _isStimulusCharge.value = value!;
-                },
-                title: const Text('척수신경 자극기 충전'),
-                controlAffinity: ListTileControlAffinity.leading,
               ),
             ),
+            _swellingInforBox,
+            _stimulusInforBox,
           ],
         ),
       ),
@@ -204,16 +100,16 @@ class _BlogCommentView extends State<BlogCommentView> {
     CommentInfo? comment = await _provider.searchComment(widget.blogIndex);
     if (comment != null) {
       _isHaveData = true;
-      _medicineInputBox.isTakeMorning = comment.takeMorning;
-      _medicineInputBox.isTakeDinner = comment.takeEvening;
-      _medicineInputBox.isTakePatch = comment.usePath;
-      _medicineInputBox.isAntiAnalgesic = comment.takeAnalogesic;
-      _medicineInputBox.isNarcoticAnalgesic = comment.takeNarcotic;
-      _commentTextController.text = comment.comment.toString();
-      _swellingController.value = _swellDropdownButton.list[comment.swellingLv];
-      _activeController.value = _activeDropDownButton.list[comment.activeMode];
-      _sleepController.value = _sleepDrowWondButton.list[comment.sleepMode];
-      _isStimulusCharge.value = comment.charging ? true : false;
+      _medicineInforBox.isTakeMorning = comment.takeMorning;
+      _medicineInforBox.isTakeDinner = comment.takeEvening;
+      _medicineInforBox.isTakePatch = comment.usePath;
+      _medicineInforBox.isAntiAnalgesic = comment.takeAnalogesic;
+      _medicineInforBox.isNarcoticAnalgesic = comment.takeNarcotic;
+      _commentTextController.text = comment.comment;
+      _swellingInforBox.value = comment.swellingLv;
+      _stimulusInforBox.activeValue = comment.activeMode;
+      _stimulusInforBox.sleepValue = comment.sleepMode;
+      _stimulusInforBox.isStimulusCharge = comment.charging ? true : false;
     } else {
       _isHaveData = false;
     }
