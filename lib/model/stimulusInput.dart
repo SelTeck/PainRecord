d63a16078ce70b9c191fdc1cddfc492fba651874
SelTeck <zbilizo@gmail.com>
2024-09-17 18:30:36 +0900

    변수 추가

--- lib/model/stimulusInput.dart
+++ lib/model/stimulusInput.dart
@@ -1,6 +1,7 @@
 import 'package:pain_record/model/result.dart';
 
 class StimulusInput extends Result {
+  late int _statusCode;
   final int number;
 
   StimulusInput({required super.message, required this.number});
@@ -8,4 +9,7 @@ class StimulusInput extends Result {
   factory StimulusInput.fromJson(Map<String, dynamic> json) {
     return StimulusInput(message: json['message'], number: json['number']);
   }
+
+  get statusCode => _statusCode;
+  set statusCode(code) => _statusCode = code;
 }
